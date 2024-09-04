8749e6a454ecb3e019eee30c758b7a1fded34144
bryansine <bryansine1738@gmail.com>
2024-09-04 14:19:31 +0300

    update

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -2,22 +2,18 @@
 import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
 
-class AppController {
-  static async getStatus(req, res) {
-    const redisStatus = redisClient.isAlive();
-    const dbStatus = dbClient.isAlive();
+export default class AppController {
+  static getStatus(req, res) {
     res.status(200).json({
-      redis: redisStatus,
-      db: dbStatus,
+      redis: redisClient.isAlive(),
+      db: dbClient.isAlive(),
     });
   }
 
-  static async getStats(req, res) {
+  static getStats(req, res) {
     Promise.all([dbClient.nbUsers(), dbClient.nbFiles()])
       .then(([usersCount, filesCount]) => {
         res.status(200).json({ users: usersCount, files: filesCount });
       });
   }
 }
-
-export default AppController;
